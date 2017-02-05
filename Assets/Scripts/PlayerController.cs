4fed1bf7b1a608c4f80a2b8b73beeb2fc92d4241
Rasmus <rasmuspuls@live.com>
2017-02-05 15:17:58 +0100

    Rasmus UFO

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;             //Floating point variable to store the player's movement speed.
+    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+	private int pickupCount;
+	public Text scoreText;
+	public Text winText;
+	
+	
+    // Use this for initialization
+    void Start() {
+        rb2d = GetComponent<Rigidbody2D> ();
+		pickupCount = 0;
+		winText.text = "";
+		SetCountText();
+    }
+
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+        Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+        rb2d.AddForce (movement * speed);
+    }
+	
+	void OnTriggerEnter2D(Collider2D other)  {
+        if (other.gameObject.CompareTag("PickUp")){
+			other.gameObject.SetActive(false);
+			pickupCount = pickupCount + 1;
+			SetCountText();
+		}
+    }
+	
+	void SetCountText(){
+        scoreText.text = "Score: " + pickupCount.ToString();
+        if (pickupCount >= 11)
+            winText.text = "Hunger & Thirst Eliminated";
+    }
+}
+
---
+++ Assets/Scripts/tiller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tiller : MonoBehaviour {
+
+	float zRotation = 0;
+	int dir = 45;
+	
+	void start(){
+		zRotation = transform.eulerAngles.z;
+	}
+	
+	void Update () {
+		transform.Rotate (new Vector3 (0, 0, dir) * Time.deltaTime);
+		zRotation = transform.eulerAngles.z;
+		if(zRotation > 15){
+			dir = -45;
+		}
+		else if(zRotation < 1){
+			dir = 45;
+		}        
+    }
+}
